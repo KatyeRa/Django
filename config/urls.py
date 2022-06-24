561af625941a3d7dc18f0b51219992056a964a16
Екатерина Ракитова <>
2022-06-24 13:33:14 +0300

    sentry

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
 from authentication.views import UserViewSet
 from api.views import TrackViewSet, PlaylistViewSet, AlbumViewSet
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 router = DefaultRouter()
 router.register('user', UserViewSet)
@@ -14,7 +16,9 @@ router.register('track', TrackViewSet)
 router.register('playlist', PlaylistViewSet)
 router.register('album', AlbumViewSet)
 
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
